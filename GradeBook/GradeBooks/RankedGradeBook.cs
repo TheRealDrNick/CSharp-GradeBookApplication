d3d84ec8b87386e9879b906bf79a47741d5e4eff
Nicolas Wittmann <Nicolas.Wittmann@ke-elektronik.de>
2020-12-18 18:01:10 +0100

    M3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,36 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException("You need at least 5 students for this operation!");
+            }
+
+            Students.Sort((x,y) => x.AverageGrade.CompareTo(y.AverageGrade));
+            var first20Percent = Students.Count / 5;
+            foreach (var student in Students)
+            {
+                var studentIndex = Students.IndexOf(student) + 1;
+                if(studentIndex <= first20Percent)
+                {
+                    return 'A';
+                }else if(studentIndex <= first20Percent * 2)
+                {
+                    return 'B';
+                }else if(studentIndex <= first20Percent * 3)
+                {
+                    return 'C';
+                }else if(studentIndex <= first20Percent * 4)
+                {
+                    return 'D';
+                }
+                
+            }
+            
+            return 'F';
+        }
     }
 }
